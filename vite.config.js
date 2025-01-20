7d0e61799e1406db9ef4cf8ea9d6635de3ee25dd
Dmitrii Kuznetsov <dkuznetsov777@gmail.com>
2025-01-20 23:04:36 +0700

    added gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), ghPages()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base: 'https://github.com/dkztv/infinite-scroll',
   build: {
     sourcemap: process.env.NODE_ENV !== 'production',
     rollupOptions: {
